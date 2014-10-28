75889842c4f072ce10da2a77c45434c7bd693aae
Watson <watson1978@gmail.com>
2014-10-28 10:56:56 +0900

    fix typo

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,7 +1,7 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     credentialsProvider = AWSStaticCredentialsProvider.credentialsWithAccessKey "foo", secretKey:"bar"
-    settings = SKTSettings.settingWithAppToken 'token'
+    settings = SKTSettings.settingsWithAppToken 'token'
     true
   end
 end
